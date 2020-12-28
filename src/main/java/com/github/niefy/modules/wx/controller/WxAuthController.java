71cdd4175a05981a65823986a11db3af12fd2828
247292980 <1095740812@qq.com>
2020-12-28 14:51:41 +0800

    20201228 注释系统日志

--- src/main/java/com/github/niefy/modules/wx/controller/WxAuthController.java
+++ src/main/java/com/github/niefy/modules/wx/controller/WxAuthController.java
@@ -1,7 +1,6 @@
 package com.github.niefy.modules.wx.controller;
 
 import com.github.niefy.common.utils.*;
-import com.github.niefy.modules.sys.service.SysLogService;
 import com.github.niefy.modules.wx.dto.WxMpInfo;
 import com.github.niefy.modules.wx.entity.WxUser;
 import com.github.niefy.modules.wx.form.WxH5OuthrizeForm;
@@ -12,7 +11,6 @@ import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
 import me.chanjar.weixin.common.bean.oauth2.WxOAuth2AccessToken;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.bean.result.WxMpUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,11 +32,12 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class WxAuthController {
     Logger logger = LoggerFactory.getLogger(this.getClass());
-    @Autowired
-    SysLogService sysLogService;
+    //    @Autowired
+//    SysLogService sysLogService;
     private final WxMpService wxMpService;
     @Autowired
     WxMpInfo wxMpInfo;
+
     /**
      * 使用微信授权code换取openid
      *
@@ -49,9 +48,9 @@ public class WxAuthController {
      */
     @PostMapping("/codeToOpenid")
     @CrossOrigin
-    @ApiOperation(value = "网页登录-code换取openid",notes = "scope为snsapi_base")
+    @ApiOperation(value = "网页登录-code换取openid", notes = "scope为snsapi_base")
     public R codeToOpenid(HttpServletRequest request, HttpServletResponse response,
-                         @RequestBody WxH5OuthrizeForm form) {
+                          @RequestBody WxH5OuthrizeForm form) {
         String appid = wxMpInfo.getAppId();
 
         try {
@@ -78,7 +77,7 @@ public class WxAuthController {
      */
     @PostMapping("/codeToUserInfo")
     @CrossOrigin
-    @ApiOperation(value = "网页登录-code换取用户信息",notes = "需scope为 snsapi_userinfo")
+    @ApiOperation(value = "网页登录-code换取用户信息", notes = "需scope为 snsapi_userinfo")
     public R codeToUserInfo(HttpServletRequest request, HttpServletResponse response,
                             @RequestBody WxH5OuthrizeForm form) {
         String appid = wxMpInfo.getAppId();
@@ -86,12 +85,12 @@ public class WxAuthController {
         try {
             this.wxMpService.switchoverTo(appid);
             WxOAuth2AccessToken token = wxMpService.getOAuth2Service().getAccessToken(form.getCode());
-            WxOAuth2UserInfo userInfo = wxMpService.getOAuth2Service().getUserInfo(token,"zh_CN");
+            WxOAuth2UserInfo userInfo = wxMpService.getOAuth2Service().getUserInfo(token, "zh_CN");
             String openid = userInfo.getOpenid();
             CookieUtil.setCookie(response, "openid", openid, 365 * 24 * 60 * 60);
             String openidToken = MD5Util.getMd5AndSalt(openid);
             CookieUtil.setCookie(response, "openidToken", openidToken, 365 * 24 * 60 * 60);
-            return R.ok().put(new WxUser(userInfo,appid));
+            return R.ok().put(new WxUser(userInfo, appid));
         } catch (WxErrorException e) {
             logger.error("code换取用户信息失败", e);
             return R.error(e.getError().getErrorMsg());
@@ -107,7 +106,7 @@ public class WxAuthController {
      * @return
      */
     @GetMapping("/getShareSignature")
-    @ApiOperation(value = "获取微信分享的签名配置",notes = "微信公众号添加了js安全域名的网站才能加载微信分享")
+    @ApiOperation(value = "获取微信分享的签名配置", notes = "微信公众号添加了js安全域名的网站才能加载微信分享")
     public R getShareSignature(HttpServletRequest request, HttpServletResponse response) throws WxErrorException {
         String appid = wxMpInfo.getAppId();
 
@@ -115,7 +114,7 @@ public class WxAuthController {
         // 1.拼接url（当前网页的URL，不包含#及其后面部分）
         String wxShareUrl = request.getHeader(Constant.WX_CLIENT_HREF_HEADER);
         if (!StringUtils.hasText(wxShareUrl)) {
-            return R.error("header中缺少"+Constant.WX_CLIENT_HREF_HEADER+"参数，微信分享加载失败");
+            return R.error("header中缺少" + Constant.WX_CLIENT_HREF_HEADER + "参数，微信分享加载失败");
         }
         wxShareUrl = wxShareUrl.split("#")[0];
         Map<String, String> wxMap = new TreeMap<>();
